7d19cccb8b87e6f2d5920fff467f6047794de46b
JantBogard <joancenkonga@gmail.com>
2022-05-24 23:08:27 +0100

    Add directive in faceSnap

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './../models/face-snap.model';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,33 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl!: string;
-  snapped!: boolean;
+  @Input() faceSnap!: FaceSnap;
   textButton!: string;
 
   ngOnInit(): void {
-    this.title = "Lucy";
-    this.description = "Is my best friend for a long time !";
-    this.createdDate = new Date;
-    this.snaps = 6;
-    this.imageUrl = "https://via.placeholder.com/300x200";
-    this.snapped = false;
     this.textButton = "Oh Snap!";
   }
 
   onAddSnap () {
-    if (!this.snapped) {
-      this.snaps++;
+    if (this.textButton === "Oh Snap!") {
+      this.faceSnap.snaps++;
       this.textButton = "Oops, un Snap!";
-      this.snapped = !this.snapped;
     } else {
-      this.snaps--;
+      this.faceSnap.snaps--;
       this.textButton = "Oh Snap!";
-      this.snapped = !this.snapped;
     }
   }
 }
