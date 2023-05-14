d3aa2fb701c98f18872df30d4c14d7c346d811b5
JantBogard <joancenkonga@gmail.com>
2023-05-14 07:27:24 +0100

    Add validators and newFacesnap function

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,8 +1,10 @@
 import { map } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FaceSnap } from '../models/face-snap.model';
 import { Observable } from 'rxjs';
+import { FaceSnapService } from '../services/face-snap.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-face-snap',
@@ -13,15 +15,23 @@ export class NewFaceSnapComponent implements OnInit {
 
   snapForm!: FormGroup;
   faceSnapPreview$!: Observable<FaceSnap>;
+  urlRegex!: RegExp;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private faceSnapService: FaceSnapService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
     this.snapForm = this.formBuilder.group({
-      title: [null],
-      description: [null],
-      imageUrl: [null],
+      title: [null, Validators.required],
+      description: [null, Validators.required],
+      imageUrl: [null, [Validators.required, Validators.pattern(this.urlRegex)]],
       location: [null]
+    }, {
+      updateOn: 'blur'
     });
 
     this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
@@ -35,7 +45,8 @@ export class NewFaceSnapComponent implements OnInit {
   }
 
   onSubmitForm() {
-    console.log(this.snapForm.value);
+    this.faceSnapService.addFaceSnap(this.snapForm.value);
+    this.router.navigateByUrl('/facesnaps');
   }
 
 }
--- src/app/services/face-snap.service.ts
+++ src/app/services/face-snap.service.ts
@@ -40,7 +40,7 @@ export class FaceSnapService {
 
   getSnapById(faceSnapId: number): FaceSnap {
     const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
-    if(faceSnap) {
+    if (faceSnap) {
       return faceSnap;
     } else {
       throw new Error("FaceSnap not found with id: " + faceSnapId);
@@ -52,5 +52,16 @@ export class FaceSnapService {
     const faceSnap = this.getSnapById(faceSnapId);
 
     snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
+  }
+
+  addFaceSnap(formValue: { title: string, description: string, imageUrl: string, location?: string }): void {
+    const faceSnap: FaceSnap = {
+      ...formValue,
+      createdDate: new Date(),
+      snaps: 0,
+      id: this.faceSnaps[this.faceSnaps.length - 1].id + 1
+    }
+
+    this.faceSnaps.push(faceSnap);
   }
 }
